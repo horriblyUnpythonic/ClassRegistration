f31f26bb620886ab20bfd68d492b82ee35106e60
brig <brockwell6@gatech.edu>
2017-12-10 16:45:25 -0800

    Create student records for when assignments are made.

--- student_registration/management/commands/calculateregistration.py
+++ student_registration/management/commands/calculateregistration.py
@@ -15,7 +15,22 @@ class Command(BaseCommand):
         example_assignments = assign_students(example_sp)
 
         semester = Semester.objects.last()
-        # StudentRecord.objects.filter()
+        current_student_records = StudentRecord.objects.filter(course_offering__semester=semester)
+        if current_student_records:
+            for record in current_student_records:
+                print record
+            choice_given = raw_input('there are currently student records from this semester, stop, add, or delete?')
+            if choice_given == 'delete':
+                current_student_records.delete()
+            elif choice_given == 'stop':
+                print 'stopping'
+                return
+            elif choice_given == 'add':
+                print 'whatever you way'
+            else:
+                print 'unrecognized'
+                return
+
 
         cp = CoursePreference.objects.filter(semester=semester)
         sp = []
@@ -26,13 +41,9 @@ class Command(BaseCommand):
                 'student-credits': pref.student.total_credits,
                 'student-id': pref.student_id,
             })
-            print sp
 
         assignments = assign_students(sp)
         for course_offering_id, student_id_list in assignments.iteritems():
             for student_id in student_id_list:
-                StudentRecord(student_id=student_id, course_offering_id=course_offering_id)
-
-        print assignments
-        return
-
+                sr = StudentRecord(student_id=student_id, course_offering_id=course_offering_id)
+                sr.save()
